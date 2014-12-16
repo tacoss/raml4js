b01c806c4def92366054b6fcb7693e239177acfc
Alvaro Cabrera <pate@patech.local>
2014-12-16 13:00:23 -0600

    Updated deps for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,3 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    watch: {
-      all: {
-        files: ['spec/**', 'lib/**'],
-        tasks: ['jshint', 'jasmine_node']
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js']
-    },
-    jasmine_node: {
-      all: ['spec'],
-      options: {
-        coffee: true
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  grunt.registerTask('default', ['jshint', 'jasmine_node']);
+  grunt.loadNpmTasks('grunt-parts');
 };
